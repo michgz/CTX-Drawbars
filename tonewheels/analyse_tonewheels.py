f52e7846acb5c181aaa285688c08c44202db8b0e
michgz <mdr.nz11@gmail.com>
2021-08-22 11:45:06 +0100

    Plays all the tones

--- tonewheels/analyse_tonewheels.py
+++ tonewheels/analyse_tonewheels.py
@@ -1,5 +1,5 @@
 '''
-Measure envelope parameters
+Measure tonewheel frequencies
 '''
 
 
@@ -15,7 +15,7 @@ import numpy
 import matplotlib.pyplot as plt
 import textwrap
 import pickle
-from fitting import fitting
+#from fitting import fitting
 import itertools
 
 import sys
@@ -29,19 +29,167 @@ from internal.sysex_comms_internal import get_single_parameter, upload_ac7_inter
 
 
 
-#p = pyaudio.PyAudio()
+if not sys.platform.startswith('linux'):
+  raise Exception("Terminating. This is an investigative script that probably needs Linux in order to work correctly.")
 
 
-#for i in range(30):
-#  print(p.get_device_info_by_index(i))
+pulse_device_index = None
 
-#print(p.get_default_host_api_info())
+p = pyaudio.PyAudio()
 
-#print(p.get_default_input_device_info())
+for i in range(p.get_device_count()):
+  d = p.get_device_info_by_index(i)
+  if d['name'] == 'pulse' and d['maxInputChannels'] >= 1:
+    pulse_device_index = i
+    break
 
-#p.terminate()
-#sys.exit(0)
+p.terminate()
 
+if pulse_device_index is None:
+  raise Exception("Terminating. Could not find pulseAudio as an input device")
+
+
+
+
+
+
+# The following are the Category 12 ("Split") numbers that sound like monophonic tonewheel
+# recordings in a CT-X3000 (probably also CT-X5000).
+#
+CAT12_VALUES = [1015,1016,1017,1019,1020,1021,1022,1027,1029,1030,1031,1033]
+
+
+# The user tone number for the experimental data. 801-900
+#
+DEST = 801
+
+
+
+
+# Set up Category 3. Start with preset data 089 ("CLICK ORGAN")
+#
+with open(os.path.join("..", "internal", "Data", "089CLICKORG.TON"), "rb") as f2:
+  y = bytearray(f2.read()[0x20:-4])
+y[0x82:0x84] = struct.pack("<H", 900)
+y[0x10A:0x10C] = struct.pack("<H", 900)
+y[0x87] = 0
+y[0x10F] = 0
+y[0x1A6:0x1B2] = "---".ljust(12, " ").encode("ascii")
+
+upload_ac7_internal(DEST-801, bytes(y), memory=1, category=3)
+
+
+
+# Set up Category 5. Start with preset data 290 ("CLICK ORGAN", used by category
+# 3 tone 089).
+#
+with open(os.path.join("..", "internal", "Data", "CAT5_290.bin"), "rb") as f2:
+  z = bytearray(f2.read())
+for i in range(8):
+  if i == 0:
+    u = 0x1CF
+  else:
+    u = 0
+  z[0+i*6:2+i*6] = struct.pack("<H", u)
+  z[2+i*6] = 0     #min velocity
+  z[3+i*6] = 0x7f  # max velocity
+  z[4+i*6] = 0     # min midi note
+  z[5+i*6] = 0x7f  # max midi note
+  
+upload_ac7_internal(0, bytes(z), memory=1, category=5)
+
+
+#   Param   Offset   Meaning
+#     0      0x42
+#     1
+#     2
+#     3      0x43
+#     4      0x00     Category 12 index
+#     5      0x02     Minimum velocity
+#     6      0x03     Maximum velocity
+#     7      0x04     Minimum MIDI note
+#     8      0x05     Maximum MIDI note
+#     9
+#    10      0x30
+#    11      0x31
+#    12      0x32
+#    13      0x33
+#    14      0x34
+#    15      0x35
+#    16      
+#    17      0x36
+#    18      0x37
+#    19      0x38
+#    20      0x39
+#    21      0x3A
+#    22      0x3B
+#    23      0x3C
+#    24      0x3D
+#    25      0x3E
+#    26      0x3F
+#    27      
+
+
+
+def get_param_length(param, category=5):
+  
+  if category != 5:
+    raise Exception("Only category 5 is implemented so far!")
+  
+  if param > 27:
+    raise Exception("Invalid parameter number for category 5")
+  
+  if param in [0, 4, 13, 20, 25]:
+    return 2
+  return 1
+
+
+
+f_midi = os.open("/dev/midi1", os.O_RDWR)
+
+
+def set_single_parameter(pp, data, category=5, memory=1, parameter_set=0, block0=0,block1=0,block2=0):
+  
+  global f_midi
+  
+  
+  b = b'\xF0\x44\x19\x01\x7F\x01' + struct.pack("BBBB", category, memory, parameter_set%128, parameter_set//128) + b'\x00\x00\x00\x00' + struct.pack("<HHHHH", block1, block0, pp, 0, 0)
+  
+  if get_param_length(pp) == 2:
+    b += struct.pack("BB", data%128, data//128)
+  else:
+    try:
+      b += struct.pack("B", data)
+    except:
+      print(f"pp={pp}")
+      print(f"data={data}")
+  b += b'\xF7'
+  
+  os.write(f_midi, b)
+
+
+for CAT12 in CAT12_VALUES:
+  
+  set_single_parameter(4, CAT12, memory=1, category=5)
+
+
+  os.write(f_midi, struct.pack("8B", 0xB0, 0x00, 65, 0xB0, 0x20, 0, 0xC0, DEST-801))
+
+  for NOTE in [36, 60, 84]:   # Three octaves of "C"
+    time.sleep(0.2)
+    os.write(f_midi, struct.pack("3B", 0x90, NOTE, 0x7F))
+    time.sleep(1.0)
+    os.write(f_midi, struct.pack("3B", 0x80, NOTE, 0x7F))
+    time.sleep(0.6)
+    
+
+
+
+
+os.close(f_midi)
+
+
+sys.exit(0)
 
 
 
